339d098c12ac58cd156788283b92b627d9873564
James Melky <james.melky@hotmail.com>
2020-06-13 09:55:27 +0930

    times up function

--- Assets/script.js
+++ Assets/script.js
@@ -64,3 +64,84 @@ var game = {
         game.loadQuestion();
 
     },
+    // remove the start button when clicked
+$('#start').on('click', function(){
+    $('#start').remove();
+  game.loadQuestion();
+
+})
+
+// click event when you click the answer
+
+$(document).on('click','.answer-button',function(e){
+    game.clicked(e);
+})
+
+$(document).on('click','#reset',function(){
+    game.reset();
+})
+
+// Variable for questions, an array of objects 
+
+var questions = [{
+    question: "Commonly used data types DO NOT include:",
+    answers: ["Strings", "Booleans", "Alerts","Numbers"],
+    correctAnswer: "Alerts",
+    image: "assets/images/imagename"
+}, {
+    question: "North Carolina grows the most what in the nation?",
+    answers: ["Tomatoes", "Sweet Potatoes", "Broccoli", "Soybeans"],
+    correctAnswer: "Sweet Potatoes",
+    image: "assets/images/imagename",
+}, {
+question: "What is the state bird?",
+    answers: ["Bald Eagle", "Blue Jay", "Cardinal", "Wild Turkey"],
+    correctAnswer: "Cardinal",
+    image: "assets/images/imagename",
+},
+
+];
+
+
+var game = {
+    questions:questions,
+    currentQuestion:0, 
+    counter:30, 
+    correct:0,
+    incorrect:0,
+    unanswered:0,
+    
+    countdown: function(){
+        game.counter --;
+        $('#counter').html(game.counter); 
+        if(game.counter<=0){
+            console.log("TIME UP!")
+            game.timeUp();
+        }
+    },
+    loadQuestion: function (){
+        timer = setInterval(game.countdown,1000);
+        $('#subwrapper').html("<h2> Time to Guess: <span id ='counter'>30</span> Seconds</h2>");
+        $('#subwrapper').append('<h2>'+questions[game.currentQuestion].question+'</h2>');
+        for(var i=0;i<questions[game.currentQuestion].answers.length;i++){
+            $('#subwrapper').append('<button class="answer-button id="button- '+i+'" data-name="'+questions[game.currentQuestion].answers[i]+'">'+questions[game.currentQuestion].answers[i]+'</button>');
+        }
+    },
+    nextQuestion: function(){
+        game.counter = 30;
+        $('#counter').html(game.counter);
+        game.currentQuestion++;
+        game.loadQuestion();
+
+    },
+    timeUp: function(){
+        clearInterval(timer);
+        game.unanswered++;
+        $('#subwrapper').html('<h2>Out of time!<h2>');
+        $('#subwrapper').append('<h3>The correct answer was: '+questions[game.currentQuestion].correctAnswer+'</h3>');
+        if(game.currentQuestion==questions.length-1){
+            setTimeout(game.results,3*1000);
+        } else{
+            setTimeout(game.nextQuestion,3*1000);
+        }
+    },
