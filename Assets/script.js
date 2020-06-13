f2619ac7b29323b33c200368a35fbec5d92a1df8
James Melky <james.melky@hotmail.com>
2020-06-13 09:44:52 +0930

    adding questions to js

--- Assets/script.js
+++ Assets/script.js
@@ -14,3 +14,21 @@ $(document).on('click','.answer-button',function(e){
 $(document).on('click','#reset',function(){
     game.reset();
 })
+
+// Variable for questions, an array of objects 
+
+var questions = [{
+    question: "Commonly used data types DO NOT include:",
+    answers: ["Strings", "Booleans", "Alerts","Numbers"],
+    correctAnswer: "Alerts",
+}, {
+    question: "HTML stands for which?",
+    answers: ["HyperText Markup Language", "HypoText Markdown Language", "HydraText Markup Language", "HyperText Markdown Lanugage"],
+    correctAnswer: "HyperText Markup Language",
+}, {
+question: "What is the state bird?",
+    answers: ["Bald Eagle", "Blue Jay", "Cardinal", "Wild Turkey"],
+    correctAnswer: "Cardinal",
+\},
+
+];
